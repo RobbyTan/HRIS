f59515513374f3e0cf180da6bcface9f61d8901f
RobbyTan <robbytanrotan@gmail.com>
2017-12-05 10:20:08 +0700

    login and select employee

---
+++ app.js
@@ -0,0 +1,27 @@
+var express	 		  = require("express"),
+app	      			  = express(),
+bodyParser 			  = require("body-parser");
+
+app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/login",function(req,res){
+	res.render("login");
+})
+app.get("/",function(req,res){
+	res.render("dashboard");
+})
+app.post("/login",function(req,res){
+	res.render("dashboard");
+})
+app.get("/employee",function(req,res){
+	res.render("./employee/statusEmployee");
+})
+app.post("/employee",function(req,res){
+	res.send(req.body.status);
+})
+app.listen(3000,function(){
+	console.log("Server has started")
+})
+
