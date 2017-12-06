0c8493334dba8c0961de4b9f6229e6ce2b0db79f
PhantomX7 <kenichixie@gmail.com>
2017-12-06 10:14:17 +0700

    add many UI improvement

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/login",function(req,res){
+app.get("/",(req,res)=>{
+	res.render("dashboard");
+})
+app.get("/login",(req,res)=>{
 	res.render("login");
 })
-app.get("/",function(req,res){
-	res.render("dashboard");
+app.post("/login",(req,res)=>{
+	res.redirect("/");
 })
-app.post("/login",function(req,res){
-	res.render("dashboard");
-})
-app.get("/employee",function(req,res){
+app.get("/employee",(req,res)=>{
 	res.render("./employee/statusEmployee");
 })
-app.post("/employee",function(req,res){
+app.post("/employee",(req,res)=>{
 	var status=req.body.status;
 	res.render("./employee/inputEmployee",{status:status});
 })
-app.listen(3000,function(){
+app.listen(3000,()=>{
 	console.log("Server has started")
 })
